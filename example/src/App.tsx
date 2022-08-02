55065f70f0c473b8c02bc7827113d8d3d45ff2ec
Felipe Calves <felipe.a.c090396@gmail.com>
2022-08-02 00:07:05 -0400

    fix: correção para yuphelper.tsx obter field recursivos

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,26 +1,51 @@
-import React from 'react';
-import { Alert, Button } from 'react-native';
+import React, { useState } from 'react';
+import { Alert, Button, Text, TextInput, View } from 'react-native';
 import { Form, Field, Watch } from 'react-native-iform';
-import Input from './components/Input';
+import * as yup from 'yup';
+
+const schema = yup.object().shape({
+  user: yup.object().shape({
+    name: yup.string().required().min(6).label('Nome'),
+  }),
+});
 
 const App = () => {
+  const [inputVisible, setInputVisible] = useState(true);
+
   const handleSubmit = (values: object) => {
     Alert.alert(JSON.stringify(values, null, 2));
   };
 
   return (
-    <Form onSubmit={handleSubmit}>
+    <Form onSubmit={handleSubmit} yupSchema={schema} shouldUnregister={false}>
       {({ onSubmit }) => (
-        <>
-          <Watch showError={true} />
-          <Field
-            name="name"
-            rules={{ required: true, minLength: 3, maxLength: 5 }}
-          >
-            <Input />
-          </Field>
+        <View>
+          <Watch />
+          {inputVisible && (
+            <Field
+              name="user.name"
+              rules={{ required: true, minLength: 3, maxLength: 5 }}
+            >
+              {({ field, fieldState, label }) => (
+                <View style={{ padding: 10 }}>
+                  <Text>{label ?? 'Outro label'}</Text>
+                  <TextInput
+                    style={{ borderWidth: 1 }}
+                    onChangeText={field.onChange}
+                    value={field.value}
+                    onBlur={field.onBlur}
+                  />
+                  <Text>{fieldState?.error?.message}</Text>
+                </View>
+              )}
+            </Field>
+          )}
           <Button title="Salvar" onPress={onSubmit} />
-        </>
+          <Button
+            title="Ocultar/Mostrar"
+            onPress={() => setInputVisible(!inputVisible)}
+          />
+        </View>
       )}
     </Form>
   );
